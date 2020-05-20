2a8563fb6f879a7bd0f4d218d127a924e32b06b4
SUN BO <starry@vip.qq.com>
2020-05-20 18:58:04 +0800

    解决一个处理css文件不正确的问题

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
             },
             {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
                 test: /\.vue$/,
                 loader: 'vue-loader',
                 options: {
