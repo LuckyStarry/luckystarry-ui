6e99e553acbed7ef589e0409786a660bdff24c2b
SUN BO <starry@vip.qq.com>
2020-05-25 13:33:05 +0800

    更换一个因为import位置导致的空指针问题

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,3 +1,4 @@
+import * as impl from './impl'
 export { Apis } from './apis'
 export { Context } from './context'
 export { Cookie } from './cookie'
@@ -5,4 +6,3 @@ export { OAuth } from './oauth'
 export { Routes } from './routes'
 export { Token } from './token'
 export { impl }
-import * as impl from './impl'
