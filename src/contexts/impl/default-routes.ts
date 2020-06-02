fb269c4322ea08e2e29742afc3367dc6fc9b8f91
SUN BO <starry@vip.qq.com>
2020-06-02 19:17:22 +0800

    支持配置二级目录

--- src/contexts/impl/default-routes.ts
+++ src/contexts/impl/default-routes.ts
@@ -4,19 +4,31 @@ import { Routes } from '../routes'
 
 export class DefaultRoutes implements Routes {
   private context: Context
+  private _baseURL: string = '/'
+  private router!: VueRouter
+  public constants: RouteConfig[] = []
+  public dynamic: RouteConfig[] = []
+
   public constructor(context: Context) {
     this.context = context
   }
 
-  private router!: VueRouter
-  public constants: RouteConfig[] = []
-  public dynamic: RouteConfig[] = []
+  public set baseURL(value: string) {
+    this._baseURL = (value || '/').trim()
+  }
+
+  public get baseURL(): string {
+    return this._baseURL
+  }
+
   public reset(): void {
     let router = this.create() as any
     let original = this.router as any
     original.matcher = router.matcher
   }
+
   public add(...route: RouteConfig[]): void {}
+
   public create(): VueRouter {
     let router = new VueRouter({
       mode: 'history',
@@ -27,7 +39,7 @@ export class DefaultRoutes implements Routes {
           return { x: 0, y: 0 }
         }
       },
-      base: process.env.BASE_URL,
+      base: this.baseURL,
       routes: this.constants
     })
     if (!this.router) {
--- src/contexts/routes.ts
+++ src/contexts/routes.ts
@@ -3,6 +3,7 @@ import VueRouter, { RouteConfig } from 'vue-router'
 export interface Routes {
   constants: RouteConfig[]
   dynamic: RouteConfig[]
+  baseURL: string
   reset(): void
   add(...route: RouteConfig[]): void
   create(): VueRouter
