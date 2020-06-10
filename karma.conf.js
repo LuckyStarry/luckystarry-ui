ef88e9136b5428407c8a29329a10643f7262ee97
SUN BO <starry@vip.qq.com>
2020-06-10 14:46:23 +0800

    初始化单元测试配置

---
+++ karma.conf.js
@@ -0,0 +1,14 @@
+const webpackConfig = require('./webpack.config.js')
+
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha'],
+    files: ['test/**/*.spec.ts'],
+    preprocessors: {
+      '**/*.spec.ts': ['webpack', 'sourcemap']
+    },
+    webpack: webpackConfig,
+    reporters: ['spec'],
+    browsers: ['Chrome']
+  })
+}
---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import index from '../src/index'
+
+describe('Test On Ready', function() {
+  it('All be fine', function() {})
+})
