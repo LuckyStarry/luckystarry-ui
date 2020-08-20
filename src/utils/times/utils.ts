fd46ea1f6a568e2a4296a42057729dacf9e6790f
SUN BO <starry@vip.qq.com>
2020-08-20 15:30:44 +0800

    完善时间工具

--- src/utils/times/utils.ts
+++ src/utils/times/utils.ts
@@ -1,14 +1,25 @@
 import { DAY, HOUR, MINUTE, SECOND } from './units'
 
+export const DEFAULT_FORMAT = '{y}-{m}-{d} {h}:{i}:{s}'
+
 export function toSmartString(time: number | Date): string {
-  let timestamp = 0
+  return getSmartDiff(time, Date.now())
+}
+
+export function getSmartDiff(time: number | Date, compared: number | Date): string {
+  let a = 0
   if (typeof time === 'number') {
-    timestamp = time
+    a = time
   } else {
-    timestamp = time.getTime()
+    a = time.getTime()
   }
-  let now = Date.now()
-  let diff = now - timestamp
+  let b = 0
+  if (typeof compared === 'number') {
+    b = compared
+  } else {
+    b = compared.getTime()
+  }
+  let diff = a - b
   let suffix = ''
   if (diff < 0) {
     suffix = '后'
@@ -18,13 +29,13 @@ export function toSmartString(time: number | Date): string {
   let desc = ''
   let abs = Math.abs(diff)
   if (abs > DAY) {
-    desc = `${Math.floor(diff / DAY)} 天`
+    desc = `${Math.floor(abs / DAY)} 天`
   } else if (abs > HOUR) {
-    desc = `${Math.floor(diff / HOUR)} 小时`
+    desc = `${Math.floor(abs / HOUR)} 小时`
   } else if (abs > MINUTE) {
-    desc = `${Math.floor(diff / MINUTE)} 分钟`
+    desc = `${Math.floor(abs / MINUTE)} 分钟`
   } else {
-    desc = `${Math.floor(diff / SECOND)} 秒`
+    desc = `${Math.floor(abs / SECOND)} 秒`
   }
   return `${desc}${suffix}`
 }
@@ -34,7 +45,7 @@ export const parseTime = (time?: Date | string | number, cFormat?: string): stri
   if (time === undefined || time === null) {
     return null
   }
-  const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+  let format = cFormat || DEFAULT_FORMAT
   let date: Date
   if (time instanceof Date) {
     date = time
@@ -47,7 +58,7 @@ export const parseTime = (time?: Date | string | number, cFormat?: string): stri
     }
     date = new Date(time)
   }
-  const formatObj: { [key: string]: number } = {
+  let payload: { [key: string]: number } = {
     y: date.getFullYear(),
     m: date.getMonth() + 1,
     d: date.getDate(),
@@ -56,13 +67,12 @@ export const parseTime = (time?: Date | string | number, cFormat?: string): stri
     s: date.getSeconds(),
     a: date.getDay()
   }
-  const timeStr = format.replace(/{([ymdhisa])+}/g, (result, key) => {
-    const value = formatObj[key]
+  return format.replace(/{([ymdhisa])+}/g, (result, key) => {
+    let value = payload[key]
     // Note: getDay() returns 0 on Sunday
     if (key === 'a') {
       return ['日', '一', '二', '三', '四', '五', '六'][value]
     }
     return value.toString().padStart(2, '0')
   })
-  return timeStr
 }
--- test/utils/times/utils.spec.ts
+++ test/utils/times/utils.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 import { expect } from 'chai'
-import { parseTime, toSmartString } from '../../../src/utils/times/utils'
+import { getSmartDiff, parseTime, toSmartString } from '../../../src/utils/times/utils'
 let offset = new Date().getTimezoneOffset()
 let hours = Math.floor(offset / 60)
 console.log(`时差：${offset} 分钟，合计：${hours} 小时`)
@@ -20,5 +20,72 @@ describe('src/utils/times/utils', function() {
 
   it('parseTime(null) => null', function() {
     expect(parseTime(null as any)).is.null
+  })
+
+  it(`parseTime('1597907865880') => '2020-08-20 15:17:45'`, function() {
+    expect(parseTime('1597907865880')).is.equals('2020-08-20 15:17:45')
+  })
+
+  it(`parseTime(1597907865880) => '2020-08-20 15:17:45'`, function() {
+    expect(parseTime(1597907865880)).is.equals('2020-08-20 15:17:45')
+  })
+
+  it(`parseTime(new Date(1597907865880)) => '2020-08-20 15:17:45'`, function() {
+    expect(parseTime(new Date(1597907865880))).is.equals('2020-08-20 15:17:45')
+  })
+
+  it(`parseTime(1597907865880, '{y}-{m}-{d} {h}:{i}:{s} | {a}') => '2020-08-20 15:17:45 | 四'`, function() {
+    expect(parseTime(1597907865880, '{y}-{m}-{d} {h}:{i}:{s} | {a}')).is.equals('2020-08-20 15:17:45 | 四')
+  })
+
+  it(`parseTime(1597907865880, '{d}/{m}/{y} {h}-{i}-{s} <{a}>') => '20/08/2020 15-17-45 <四>'`, function() {
+    expect(parseTime(1597907865880, '{d}/{m}/{y} {h}-{i}-{s} <{a}>')).is.equals('20/08/2020 15-17-45 <四>')
+  })
+
+  it('getSmartDiff(0, 0) => 0 秒前', function() {
+    expect(getSmartDiff(0, 0)).is.equals('0 秒前')
+  })
+
+  it('getSmartDiff(1000, 0) => 1 秒前', function() {
+    expect(getSmartDiff(1000, 0)).is.equals('1 秒前')
+  })
+
+  it('getSmartDiff(0, 1000) => 1 秒后', function() {
+    expect(getSmartDiff(0, 1000)).is.equals('1 秒后')
+  })
+
+  it('getSmartDiff(new Date(), new Date(Date.now() + 60000)) => 60 秒后', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now), new Date(now + 60000))).is.equals('60 秒后')
+  })
+
+  it('getSmartDiff(new Date(), new Date(Date.now() + 60001)) => 1 分钟后', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now), new Date(now + 60001))).is.equals('1 分钟后')
+  })
+
+  it('getSmartDiff(new Date(Date.now() + 3600000), new Date()) => 60 分钟前', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now + 3600000), new Date(now))).is.equals('60 分钟前')
+  })
+
+  it('getSmartDiff(new Date(Date.now() + 3600001), new Date()) => 1 小时前', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now + 3600001), new Date(now))).is.equals('1 小时前')
+  })
+
+  it('getSmartDiff(new Date(Date.now() + 86400000), new Date()) => 24 小时前', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now + 86400000), new Date(now))).is.equals('24 小时前')
+  })
+
+  it('getSmartDiff(new Date(Date.now() + 86400001), new Date()) => 1 天前', function() {
+    let now = Date.now()
+    expect(getSmartDiff(new Date(now + 86400001), new Date(now))).is.equals('1 天前')
+  })
+
+  it('toSmartString(new Date(Date.now() + 86400001)) => 1 天前', function() {
+    let now = Date.now()
+    expect(toSmartString(new Date(now + 86400001))).is.equals('1 天前')
   })
 })
