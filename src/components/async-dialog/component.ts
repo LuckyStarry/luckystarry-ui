9367fd90896bda2d136c7688ba8ecd8490f13baf
SUN BO <starry@vip.qq.com>
2021-03-04 11:37:12 +0800

    优化异步窗口逻辑

--- src/components/async-dialog/component.ts
+++ src/components/async-dialog/component.ts
@@ -1,44 +1,119 @@
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
+interface Context<T = any, U = T> {
+  title?: string
+  load?: () => Promise<T>
+  process?: () => Promise<U>
+}
+
 @Component({ name: AsyncDialog.name })
-export default class AsyncDialog<T = any> extends Vue {
+export default class AsyncDialog<T = any, U = T> extends Vue {
   @Prop({ type: String, default: '' })
   public title!: string
   @Prop({ type: String, default: '800px' })
   public width!: string
+  @Prop({ type: Boolean, default: false })
+  public closeOnClickModal?: boolean
+  @Prop({ type: Boolean, default: false })
+  public closeOnPressEscape?: boolean
 
-  public visible: boolean = false
+  private visible: boolean = false
+  private loading: boolean = false
+  private processing: boolean = false
+  private context: Context<T, U> = {}
+  private subject: T = null as any
 
-  private resolve: ((value?: T | Promise<T> | undefined) => void) | null = null
-  private reject: ((reason?: any) => void) | null = null
+  private get TITLE(): string {
+    if (this.context.title) {
+      return this.context.title
+    }
+    if (this.title) {
+      return this.title
+    }
+    return ''
+  }
 
-  public async open(payload?: any): Promise<T> {
+  private resolve?: (value: U | Promise<U>) => void
+  private reject?: (reason?: any) => void
+  private payload?: T
+
+  public async open(context: Context): Promise<U> {
+    this.context = context || {}
+    this.reject = undefined
+    this.resolve = undefined
     return new Promise(async (resolve, reject) => {
-      this.resolve = resolve
-      this.reject = reject
-
+      this.processing = false
+      this.loading = true
       this.visible = true
-      this.$emit('on-open', { payload, actions: { ok: () => this.ok(), close: () => this.close() } })
+      let legal = true
+      if (this.context.load) {
+        try {
+          let subject = await this.context.load()
+          if (subject) {
+            this.subject = subject
+          }
+        } catch (error) {
+          legal = false
+          if (error) {
+            console.error(error)
+          }
+        }
+      }
+      if (legal) {
+        this.resolve = resolve
+        this.reject = reject
+        this.loading = false
+      } else {
+        this.visible = false
+        reject()
+      }
     })
   }
 
-  public async ok(payload?: T) {
-    let resolve = this.resolve
-    if (resolve) {
-      resolve(payload)
+  public async ok() {
+    let legal = false
+    let subject: T | U = this.subject
+    if (this.context.process) {
+      try {
+        this.loading = true
+        this.processing = true
+        subject = await this.context.process()
+        legal = true
+      } catch (error) {
+        if (error) {
+          console.error(error)
+        }
+      } finally {
+        this.processing = false
+        this.loading = false
+      }
+    } else {
+      legal = true
     }
-    await this.close()
+
+    if (legal) {
+      let resolve = this.resolve
+      this.resolve = undefined
+      this.reject = undefined
+
+      if (this.visible) {
+        this.visible = false
+      }
+
+      if (resolve) {
+        resolve(subject as U)
+      }
+    }
   }
 
   public async close() {
     let reject = this.reject
+    this.resolve = undefined
+    this.reject = undefined
 
     if (this.visible) {
       this.visible = false
     }
-
-    this.resolve = null
-    this.reject = null
 
     if (reject) {
       reject()
--- src/components/async-dialog/component.vue
+++ src/components/async-dialog/component.vue
@@ -1,14 +1,24 @@
 <template>
   <el-dialog
     class="async-dialog"
-    :title="title"
+    :title="TITLE"
     :visible.sync="visible"
     :width="width"
     @close="close"
-    :close-on-click-modal="false"
-    :close-on-press-escape="false"
+    :close-on-click-modal="closeOnClickModal && !processing"
+    :close-on-press-escape="closeOnPressEscape && !processing"
   >
-    <slot :ok="ok" :close="close"></slot>
+    <slot name="full" :subject="subject" :loading="loading" :processing="processing" :ok="ok" :close="close">
+      <div class="async-dialog-panel" v-loading="loading" :element-loading-text="(processing ? '处理中' : '读取中') + '，请稍候……'">
+        <div class="async-dialog-content">
+          <slot :subject="subject" :loading="loading" :processing="processing" :ok="ok" :close="close"> </slot>
+        </div>
+        <div class="async-dialog-buttons">
+          <el-button @click="close">取消 (ESC)</el-button>
+          <el-button type="primary" @click="ok">确认</el-button>
+        </div>
+      </div>
+    </slot>
   </el-dialog>
 </template>
 <script lang="ts" src="./component.ts" />
